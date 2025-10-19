92455b41b496e8f4efa28679f2c22f0f2ed61eb2
Azure2212 <Dasanbob22122002@gmail.com>
2025-10-19 17:35:31 -0400

    update

--- MakeAction.py
+++ MakeAction.py
@@ -23,5 +23,5 @@ def on_join(data):
         print(f"joined IDRobot: {IDRobot}")
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, host='0.0.0.0', port=5000)
     print('start')
